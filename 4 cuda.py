ad63a7ced88d40295b7b48b7178cf5821501cf1f
Jim Shi <shiyilin2008@gmail.com>
2020-02-04 20:39:54 -0800

    Improvements to the CUDA test.

--- 4 cuda.py
+++ 4 cuda.py
@@ -8,7 +8,8 @@ x = torch.ones(3, 3)
 if (torch.cuda.is_available()):
     device = torch.device("cuda")  # a CUDA device object
     y = torch.ones_like(x, device=device)  # directly create a tensor on GPU
-    x = x.to(device)  # or just use strings ``.to("cuda")``
+    # x = x.to(device)  # or just use strings ``.to("cuda")``
+    x = x.cuda()    # This works as well.
     z = x + y
     print(z)
     print(z.to("cpu", torch.double))  # ``.to`` can also change dtype together!
